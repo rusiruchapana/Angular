97b540c72b440a48488a41abf08fbf5b1d6b1234
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-16 18:55:56 +0530

    complete simply counter function.

--- Learn_Angular/src/app/home/home.component.ts
+++ Learn_Angular/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component , signal } from '@angular/core';
 import {GreetingComponent} from '../greeting/greeting.component';
 import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
+import {CounterComponent} from '../counter/counter.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    GreetingComponent
+    GreetingComponent,
+    CounterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
